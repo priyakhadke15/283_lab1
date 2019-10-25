68200e309ec15a925357e263ab6f9c70e60bc39e
priya <priyakhadke15@gmail.com>
2019-10-25 10:59:32 -0700

    Hypercall code change

---
+++ linux/debian/hdrtmp/usr/src/linux-headers-5.4.0-rc4-custom/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP Thu Oct 24 14:40:28 PDT 2019"
+#define LINUX_COMPILE_BY "priya"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 8.3.0 (Ubuntu 8.3.0-6ubuntu1)"
